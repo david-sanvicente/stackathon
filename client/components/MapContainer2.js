ff3a07a25f5f348c9af75341ef93951328df2ec6
David-Sanvicente <sanvicented1@gmail.com>
2019-11-15 18:41:50 -0500

    markers are now clickable and display infowindow

--- client/components/MapContainer2.js
+++ client/components/MapContainer2.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react'
 import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react'
-import * as artData from '/Users/id/Desktop/stackathon/client/data/jersey-city-mural-map-list.json'
-const key = process.env.REACT_APP_GOOGLE_KEY
-const data = [artData]
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+}
 
 export class MapContainer extends Component {
   constructor(props) {
@@ -31,29 +33,7 @@ export class MapContainer extends Component {
     }
   }
 
-  displayMarkers = () => {
-    const mapSpots = data[0].default
-    return mapSpots.map((spot, index) => {
-      return (
-        <Marker
-          key={index}
-          id={index}
-          position={{
-            lat: spot.fields.latitude,
-            lng: spot.fields.longitude
-          }}
-          onClick={this.onMarkerClick}
-          name="Kenyatta International Convention Centre"
-        />
-      )
-    })
-  }
-
   render() {
-    const mapStyles = {
-      width: '100%',
-      height: '100%'
-    }
     return (
       <Map
         google={this.props.google}
@@ -61,7 +41,10 @@ export class MapContainer extends Component {
         style={mapStyles}
         initialCenter={{lat: 40.726691, lng: -74.059252}}
       >
-        {this.displayMarkers()}
+        <Marker
+          onClick={this.onMarkerClick}
+          name="Kenyatta International Convention Centre"
+        />
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
@@ -76,9 +59,6 @@ export class MapContainer extends Component {
   }
 }
 
-const googleWrapper = GoogleApiWrapper({
+export default GoogleApiWrapper({
   apiKey: 'AIzaSyA4BBHjmNNTbfJWTLeDqBBGAJ0ITQxU_Ko'
-  // apiKey: key
 })(MapContainer)
-
-export default googleWrapper
